456128d1c4b1b8dc717c8cee87ac2c77afbdbd7e
Derek Xu <derekxu888@gmail.com>
2021-04-13 12:10:00 -0700

    more stuff

--- Elevens/CardTester.java
+++ Elevens/CardTester.java
@@ -16,6 +16,17 @@ public class CardTester
     
     public static void main(String[] args) 
     {
-        /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        Card card1 = new Card("jack", "hearts", 11);
+        Card card2 = new Card("none", "spades", 4);
+        Card card3 = new Card("none", "spades", 4);
+
+        System.out.println(card1.toString());
+        System.out.println(card2.toString());
+        System.out.println(card3.toString());
+
+        System.out.println(card2.matches(card3));
+        System.out.println(card3.matches(card1));
+        
+        
     }
 }
